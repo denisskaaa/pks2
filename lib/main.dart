be1a72183a71cd63b503fdb7a1a26e3fa8807434
denisskaaa <den.kamenev.2004@mail.ru>
2024-12-26 23:19:38 +0300

    первый коммит

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Карточки волейболистов',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
